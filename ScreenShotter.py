9b0e3318aa014251bdeb2f148c4d8b29663a3bca
Ali El khattabi <60844044+AliElkhattabi99@users.noreply.github.com>
2023-06-01 00:39:08 +0200

    Update ScreenShotter.py

--- ScreenShotter.py
+++ ScreenShotter.py
@@ -24,6 +24,7 @@ class ScreenShotter:
                 + ".png"
             )
             github.save_to_repo("Screens/" + name, content)
+            print("Screenshot done"),
         os.remove("screenshot.png")
 
 
