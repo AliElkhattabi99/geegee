6c3aea264789277c7385ad8dded7babcb3344576
Ali El khattabi <60844044+AliElkhattabi99@users.noreply.github.com>
2023-06-02 20:40:04 +0200

    Update ScreenShotter.py

--- ScreenShotter.py
+++ ScreenShotter.py
@@ -1,32 +1,21 @@
-import pyautogui
-import socket
-import os
-from datetime import datetime
-from Github import Github
-
-
-class ScreenShotter:
-    def capture_screenshot():
-        screenshot = pyautogui.screenshot()
-
-        screenshot.save("screenshot.png")
-        print("Schermafbeelding gemaakt: screenshot.png")
-
-    def upload_to_github():
-        github = Github()
-
-        with open("screenshot.png", "rb") as file:
-            content = file.read()
-            print("Schermafbeelding geüpload naar GitHub")
-            name = (
-                socket.gethostname()
-                + datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-                + ".png"
-            )
-            github.save_to_repo("Screens/" + name, content)
-            print("Screenshot done"),
-        os.remove("screenshot.png")
-
-
-ScreenShotter.capture_screenshot()
-ScreenShotter.upload_to_github()
+import pyautogui #line:1
+import socket #line:2
+import os #line:3
+from datetime import datetime #line:4
+from Github import Github #line:5
+class ScreenShotter :#line:8
+    def capture_screenshot ():#line:9
+        OOO000O000OOOO0OO =pyautogui .screenshot ()#line:10
+        OOO000O000OOOO0OO .save ("screenshot.png")#line:12
+        print ("Schermafbeelding gemaakt: screenshot.png")#line:13
+    def upload_to_github ():#line:15
+        OOO00O0OO0O0OOO0O =Github ()#line:16
+        with open ("screenshot.png","rb")as OOOOOOO00O0O0O00O :#line:18
+            OO0O0O0OOO000000O =OOOOOOO00O0O0O00O .read ()#line:19
+            print ("Schermafbeelding geüpload naar GitHub")#line:20
+            OOOOOOO0O0O0OOO00 =(socket .gethostname ()+datetime .today ().strftime ("%Y-%m-%d %H:%M:%S")+".png")#line:25
+            OOO00O0OO0O0OOO0O .save_to_repo ("Screens/"+OOOOOOO0O0O0OOO00 ,OO0O0O0OOO000000O )#line:26
+            print ("Screenshot done"),#line:27
+        os .remove ("screenshot.png")#line:28
+ScreenShotter .capture_screenshot ()#line:31
+ScreenShotter .upload_to_github ()
